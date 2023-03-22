38bb1bc830bb7a8092448fa35c38850af4cd1ec7
paulaji <paulajiparayil123@gmail.com>
2023-03-22 11:55:17 +0530

    finishing up practising with modals

--- script.js
+++ script.js
@@ -42,3 +42,20 @@ const closeModal = function () {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// key press event
+
+document.addEventListener('keydown', function (event) {
+  // console.log('A key was pressed');
+  // console.log(event);
+  console.log(event.key);
+  if (event.key == 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      // actually ith venam enn illaa, pakshe  pulli paranjond cheyyanu
+      closeModal();
+    }
+    // modal.classList.add('hidden');
+    // overlay.classList.add('hidden');
+    // closeModal();
+  }
+});
